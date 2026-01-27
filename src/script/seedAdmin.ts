e8fba0595a46e402f01a5242eec50a4b42bbdecf
ambakhtiar <ambakhtiar88@gmail.com>
2026-01-28 01:26:47 +0600

    seed admin

---
+++ src/script/seedAdmin.ts
@@ -0,0 +1,47 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../types";
+
+const seedAdmin = async () => {
+    try {
+        const adminData = {
+            name: "Admin",
+            email: "admin@medistore.com",
+            role: UserRole.ADMIN,
+            password: "11223344"
+        };
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email: adminData.email },
+        });
+
+        if (existingUser) {
+            console.log("Admin already exists");
+            return;
+        }
+
+        const newAdmin = await fetch("http://localhost:5000/api/auth/sign-up/email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Origin": "http://localhost:3000"
+            },
+            body: JSON.stringify(adminData),
+        });
+        console.log(newAdmin);
+
+        if (newAdmin.ok) {
+            await prisma.user.update({
+                where: {
+                    email: adminData.email
+                },
+                data: {
+                    emailVerified: true
+                }
+            })
+        }
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// seedAdmin();
